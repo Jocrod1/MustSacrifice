bd65ff2b7275ac555a44755e1198f33979d0e40c
Jose Arturo <WallbangStudios4.9@gmail.com>
2018-12-02 18:25:25 -0400

    Fades y efectos

--- Assets/Scripts/Warp.cs
+++ Assets/Scripts/Warp.cs
@@ -6,14 +6,55 @@ public class Warp : MonoBehaviour {
 
     public GameObject Target;
 
+    bool start = false;
+    bool isfadein = false;
+    float alpha = 0;
+    public float fadetime = 1f;
+
+    float vel;
     void Awake() {
         GetComponent<SpriteRenderer>().enabled = false;
         transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
     }
 
-    void OnTriggerEnter2D(Collider2D other) {
+    IEnumerator OnTriggerEnter2D(Collider2D other) {
+        FadeIn();
+
+        yield return new WaitForSeconds(fadetime);
         if (other.tag == "Player") {
             other.transform.position = Target.transform.GetChild(0).transform.position;
         }
+        FadeOut();
     }
+
+    void OnGUI() {
+        if (!start)
+            return;
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+
+        Texture2D tex;
+
+        tex = new Texture2D(1, 1);
+        tex.SetPixel(0, 0, Color.black);
+        tex.Apply();
+
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), tex);
+
+        if (isfadein)
+        {
+            alpha = Mathf.SmoothDamp(alpha, 1.1f, ref vel, fadetime);
+        }
+        else {
+            alpha = Mathf.SmoothDamp(alpha, -0.1f, ref vel, fadetime);
+            if (alpha < 0) start = false;
+        }
+    }
+
+    void FadeIn() {
+        start = true;
+        isfadein = true;
+    }
+    void FadeOut() {
+        isfadein = false;
+    }   
 }
